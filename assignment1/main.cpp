55e6183b513ab3a950be4dd284b67fd10502ee21
jjoeldaniel <joeldanielrico@csu.fullerton.edu>
2023-09-01 23:38:11 -0700

    add exit message

--- assignment1/main.cpp
+++ assignment1/main.cpp
@@ -2,7 +2,13 @@
 
 int main()
 {
-    std::cout << "Welcome to Trip Advisor by Richard Nguyen.\nWe help you plan your trip."
+    std::cout << "Welcome to Trip Advisor by Richard Nguyen.\nWe help you plan your trip.\n"
               << std::endl;
+
+    // TODO : take input from calculator
+    int num = 0;
+    std::cout << "The main module recieved this number " << num << "and will keep it for a while." << std::endl;
+    std::cout << "A zero will be sent to your operating sytem.\nGood-bye. Have a safe trip." << std::endl;
+
     return 0;
 }
