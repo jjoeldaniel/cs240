4e567cadf132202ef5af2d569bd0cdb4bb3932c2
jjoeldaniel <joeldanielrico@csu.fullerton.edu>
2023-09-02 00:59:56 -0700

    add more io

--- assignment1/calculator.asm
+++ assignment1/calculator.asm
@@ -4,11 +4,19 @@ section .data
     miles db "For how many miles will you maintain this average speed? ", 0
     final_speed db "What will be your speed during the final segment of the trip (mph)? ", 0
 
+    average_speed db "Your average speed will be ", 0
+    total_time db "Your total time will be ", 0
+
+    newline db "", 10
+    test_data db "1", 10
+
 section .bss
 
     initial_speed_res resb 16
     miles_res resb 16
     final_speed_res resb 16
+    average_speed_res resb 16
+    total_time_res resb 16
 
 section .text
     global _start
@@ -27,11 +35,52 @@ _start:
     ; call _printFinalSpeed
 
     ; TODO: calculate average
+    call _printAverage
 
 
+    call _quit
+
+_quit:
     mov rax, 60
     mov rdi, 0
     syscall
+
+_newline:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, newline
+    mov rdx, 1
+    syscall
+    ret
+
+_printAverage:
+    call _newline
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, average_speed
+    mov rdx, 27
+    syscall
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, test_data
+    mov rdx, 2
+    syscall
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, total_time
+    mov rdx, 24
+    syscall
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, test_data
+    mov rdx, 2
+    syscall
+
+    ret
 
 _promptSpeed:
     mov rax, 1
