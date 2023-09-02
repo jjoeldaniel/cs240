320d8f1403deb298ef06570cbccc2238fd516bec
jjoeldaniel <joeldanielrico@csu.fullerton.edu>
2023-09-02 01:22:54 -0700

    comment out main.cpp

--- assignment1/run.sh
+++ assignment1/run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-# g++ -o main main.cpp
-# ./main
-
 nasm -f elf64 -o calculator.o calculator.asm
 ld -o calculator calculator.o
 ./calculator
+
+# g++ -o main main.cpp
+# ./main
