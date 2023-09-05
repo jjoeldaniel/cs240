22577785d9bcfb78f8e444de7f56256801903c92
jjoeldaniel <joeldanielrico@csu.fullerton.edu>
2023-09-05 00:41:31 -0700

    test

--- assignment1/main.cpp
+++ assignment1/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
-extern "C" double calculate();
+extern "C" long long calculate();
 
 int main()
 {
     std::cout << "Welcome to Trip Advisor by Joel Daniel Rico.\n"
               << "We help you plan your trip.\n\n";
 
-    double number = calculate();
+    long long number = calculate();
 
     std::cout << "\nThe main module received this number " << number << " and will keep it for a while.\n"
               << "A zero will be sent to your operating system.\n"
