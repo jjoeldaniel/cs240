bf5e27e9046cedf59ab7605c0d5ba3c48cb4cac6
jjoeldaniel <joeldanielrico@csu.fullerton.edu>
2023-09-04 23:41:22 -0700

    fix sh

--- assignment1/run.sh
+++ assignment1/run.sh
@@ -1,9 +1,25 @@
-#!/usr/bin/env bash
+# Program Name: CPSC 240 Assignment 1 - Compute Trip
 
-rm -f main
+
+# Removes all previous object files and output files
 rm -f *.o
+rm -f *.lis
+rm -f compute-trip
 
-nasm -f elf64 -o calculator.o calculator.asm
-g++ -o main main.cpp
-g++ -m64 -o main main.cpp calculator.o
-./main
+# Compiles asm file
+echo "Assembling the file compute_trip.asm"
+nasm -f elf64 -l compute-trip.lis -o asm-output.o compute_trip.asm 
+
+# Compiles cpp filea
+echo "Compiling the file main.cpp"
+g++ -c -m64 -Wall main.cpp -o cpp-output.o -fno-pie -no-pie -std=c++17
+
+# Links the two object files
+echo "Linking the object files"
+g++ -m64 asm-output.o cpp-output.o -o compute-trip -fno-pie -no-pie -std=c++17
+
+
+echo "Running the program compute_trip"
+echo ""
+# Runs the program
+./compute-trip
