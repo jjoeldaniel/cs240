f1d87afa954506cd4a253707fdac152f1558fb13
Joel Daniel Rico <joeldanielrico@gmail.com>
2023-09-22 11:25:25 -0700

    update run.sh

--- assignment2/run.sh
+++ assignment2/run.sh
@@ -1,28 +1,27 @@
+#!/usr/bin/env bash
+
 # Joel Daniel Rico
 # CPSC 240
 # Section 01
-# Program Name: CPSC 240 Assignment 1 - Compute Trip
-
+# Program Name: CPSC 240 Assignment 2 - Arrays
 
 # Removes all previous object files and output files
 rm -f *.o
 rm -f *.lis
-rm -f compute-trip
 
-# Compiles asm file
-echo "Assembling the file compute_trip.asm"
-nasm -f elf64 -l compute-trip.lis -o asm-output.o compute_trip.asm 
+# Compile main.c
+C_FILE="main"
+echo "Compile $C_FILE"
+gcc -o $C_FILE.o $C_FILE.c -std=c17
 
-# Compiles cpp filea
-echo "Compiling the file main.cpp"
-g++ -c -m64 -Wall main.cpp -o cpp-output.o -fno-pie -no-pie -std=c++17
+# Assemble *.asm files
+for file in *; do
+	if [[ $file == *.asm ]]
+	then
+		# nasm -f elf64 -o "${file}".o "${file}".asm
+		echo "${file}"
+	fi
+done
 
-# Links the two object files
-echo "Linking the object files"
-g++ -m64 asm-output.o cpp-output.o -o compute-trip -fno-pie -no-pie -std=c++17
-
-
-echo "Running the program compute_trip"
-echo ""
-# Runs the program
-./compute-trip
+chmod +x ./main.o
+./main.o
