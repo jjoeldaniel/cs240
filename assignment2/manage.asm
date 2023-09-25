939024a7e25802cd91402fc54d1f803f92875a92
Joel Daniel Rico <joeldanielrico@gmail.com>
2023-09-24 19:58:56 -0700

    finish

--- assignment2/manage.asm
+++ assignment2/manage.asm
@@ -6,7 +6,6 @@ extern scanf, printf
 extern input_array
 extern output_array
 extern sum
-extern exit
 
 section .data
     message1 db "We will take care of all your array needs.", 10, 0
@@ -32,41 +31,41 @@ section .bss
 %endmacro
 
 %macro backup 0
-    push rbp                                          ;Backup rbp
-    mov  rbp,rsp                                      ;The base pointer now points to top of stack
-    push rdi                                          ;Backup rdi
-    push rsi                                          ;Backup rsi
-    push rdx                                          ;Backup rdx
-    push rcx                                          ;Backup rcx
-    push r8                                           ;Backup r8
-    push r9                                           ;Backup r9
-    push r10                                          ;Backup r10
-    push r11                                          ;Backup r11
-    push r12                                          ;Backup r12
-    push r13                                          ;Backup r13
-    push r14                                          ;Backup r14
-    push r15                                          ;Backup r15
-    push rbx                                          ;Backup rbx
+    push rbp
+    mov  rbp,rsp
+    push rdi
+    push rsi
+    push rdx
+    push rcx
+    push r8
+    push r9
+    push r10
+    push r11
+    push r12
+    push r13
+    push r14
+    push r15
+    push rbx
     pushf
     push qword 0
 %endmacro
 
 %macro restore 0
-    popf                                    ;Restore rflags
-    pop rbx                                 ;Restore rbx
-    pop r15                                 ;Restore r15
-    pop r14                                 ;Restore r14
-    pop r13                                 ;Restore r13
-    pop r12                                 ;Restore r12
-    pop r11                                 ;Restore r11
-    pop r10                                 ;Restore r10
-    pop r9                                  ;Restore r9
-    pop r8                                  ;Restore r8
-    pop rcx                                 ;Restore rcx
-    pop rdx                                 ;Restore rdx
-    pop rsi                                 ;Restore rsi
-    pop rdi                                 ;Restore rdi
-    pop rbp                                 ;Restore rbp
+    popf
+    pop rbx
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop r11
+    pop r10
+    pop r9
+    pop r8
+    pop rcx
+    pop rdx
+    pop rsi
+    pop rdi
+    pop rbp
 %endmacro
 
 section .text
