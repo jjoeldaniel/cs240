13454a6ab19f8d217b299d76d79de7af1c586a8b
Joel Daniel Rico <joeldanielrico@gmail.com>
2023-10-15 21:37:35 -0700

    done

---
+++ assignment3/sort_pointers.asm
@@ -0,0 +1,147 @@
+; ****************************************************************************************************************************
+; Program name: "Array Sorter". This program takes an array input, sorts it, and prints the result                           *
+; Copyright (C) 2023  Joel Daniel Rico                                                                                       *
+;                                                                                                                            *
+; This file is part of the software program "Array Sorter".                                                                  *                         
+; "Array Sorter" is free software: you can redistribute it and/or modify it under the terms of the GNU General Public        *
+; License version 3 as published by the Free Software Foundation.                                                            *
+; This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied         *
+; warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.     *
+; A copy of the GNU General Public License v3 has been distributed with this software.  If not found it is available here:   *
+; <https://www.gnu.org/licenses/>.   The copyright holder may be contacted here: holliday@fullerton.edu                      *
+; ****************************************************************************************************************************
+;
+;
+; ========1=========2=========3=========4=========5=========6=========7=========8=========9=========0=========1=========2=========3**
+; Author information
+;  Name: Joel Daniel Rico
+;  CWID: 885687517
+;  Email: joeldanielrico@csu.fullerton.edu
+;
+; Program information
+;  Program name: Array Sorter
+;  Programming languages: One module in C++ one module in C, two modules in X86-64, and one module in Bash.
+;  Date program began: 2023-Oct-6
+;  Date of last update: 2023-Oct-8
+;  Files in this program: director.asm, input_array.asm, main.c, output_array.c, rx.sh, sort_pointers.c
+;  Status: This program was tested over a dozen times on Fedora 38 (Oct 2023) without errors.
+;  Purpose: The program takes an array input, sorts it, and prints the result
+;
+; This file
+;  Name: sort_pointers.asm
+;  Language: X86-64
+;  Purpose: Sorts an array of pointers
+;
+; ========1=========2=========3=========4=========5=========6=========7=========8=========9=========0=========1=========2=========3**
+
+
+segment .bss
+    align          64
+    backup_r  resb 832
+
+%macro backup 0
+    push    rbp
+    mov     rbp, rsp
+    push    rbx
+    push    rcx
+    push    rdx
+    push    rsi
+    push    rdi
+    push    r8
+    push    r9
+    push    r10
+    push    r11
+    push    r12
+    push    r13
+    push    r14
+    push    r15
+    pushf
+
+    mov         rax, 7
+    mov         rdx, 0
+    xsave       [backup_r]
+%endmacro
+
+%macro restore 0
+    popf
+    pop     r15
+    pop     r14
+    pop     r13
+    pop     r12
+    pop     r11
+    pop     r10
+    pop     r9
+    pop     r8
+    pop     rdi
+    pop     rsi
+    pop     rdx
+    pop     rcx
+    pop     rbx
+    pop     rbp
+%endmacro
+
+segment .text
+    global sort_pointers
+
+sort_pointers:
+    backup
+
+    ; Parameters
+    mov r8, rdi  ; Array Pointer
+    mov r9, rsi  ; Maximnum Array Size
+    xor r10, r10  ; Loop Counter
+    inc r10
+
+    xor r11, r11
+    xor r12, r12
+    xor r13, r13
+    
+    jmp sweep
+
+restart:
+    xor r10, r10
+    inc r10
+    xor r11, r11
+    inc r11
+    jmp sweep
+
+sweep:
+
+    ; if sorted, jump to end
+    cmp r11, r9
+    je done
+
+    ; if true, restart sweep
+    cmp r10, r9
+    jge restart
+
+    ; if swap necessary, start by temp storing
+    mov r12, [r8 + r10 * 8 - 8]
+    mov r13, [r8 + r10 * 8]
+    movsd xmm8, [r12]
+    movsd xmm9, [r13]
+
+    ; compare, swap if necessary
+    ucomisd xmm8, xmm9
+    ja swap
+
+    ; increment if no swap
+    inc r11
+    jmp after_swap
+
+swap:
+    mov r12, [r8 + r10 * 8 - 8]
+    mov r13, [r8 + r10 * 8]
+    mov [r8 + r10 * 8 - 8], r13
+    mov [r8 + r10 * 8], r12
+
+    jmp after_swap
+
+after_swap:
+    ; increment loop counter
+    inc r10
+    jmp sweep
+
+done:
+    restore
+    ret
