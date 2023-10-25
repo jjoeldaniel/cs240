b5dc27d01d6bec1e2c451c9ad8fa3b607b6367d9
Joel Daniel Rico <joeldanielrico@gmail.com>
2023-10-25 15:07:47 -0700

    fix input_array

--- midterm/input_array.asm
+++ midterm/input_array.asm
@@ -9,40 +9,48 @@ segment .data
     float_format db "%lf", 0
 
 %macro backup 0
-    push rbp
-    mov  rbp,rsp
-    push rdi
-    push rsi
-    push rdx
-    push rcx
-    push r8
-    push r9
-    push r10
-    push r11
-    push r12
-    push r13
-    push r14
-    push r15
-    push rbx
-    pushf
+        ; backup registers
+        push rbp
+        mov rbp, rsp
+        push rbx
+        push rcx
+        push rdx
+        push rsi
+        push rdi
+        push r8
+        push r9
+        push r10
+        push r11
+        push r12
+        push r13
+        push r14
+        push r15
+        pushf
+
+        ; xsave
+        mov rax, 7
+        mov rdx, 0
+        xsave [Save]
 %endmacro
 
 %macro restore 0
-    popf
-    pop rbx
-    pop r15
-    pop r14
-    pop r13
-    pop r12
-    pop r11
-    pop r10
-    pop r9
-    pop r8
-    pop rcx
-    pop rdx
-    pop rsi
-    pop rdi
-    pop rbp
+        xrstor [Save]
+
+        popf
+        pop r15
+        pop r14
+        pop r13
+        pop r12
+        pop r11
+        pop r10
+        pop r9
+        pop r8
+        pop rdi
+        pop rsi
+        pop rdx
+        pop rcx
+        pop rbx
+        pop rbp
 %endmacro
 
 %macro input 0
@@ -52,6 +60,10 @@ segment .data
     mov rsi, rsp
     call scanf 
 %endmacro
+
+segment .bss
+    align 64
+    Save resb 832
 
 segment .text
 input_array:
